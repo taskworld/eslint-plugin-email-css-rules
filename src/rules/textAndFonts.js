6563c31fc3a09f78cc4118a692b930403f1b1671
Kawin Glomjai <kawin.g@taskworld.com>
2017-08-15 11:52:28 +0700

    let's test passed

--- src/rules/textAndFonts.js
+++ src/rules/textAndFonts.js
@@ -1,18 +1,53 @@
 const { hasProp, getPropValue, elementType, getProp, propName } = require('jsx-ast-utils')
+const supportMatrix = require('../assets/supportMatrix.json')
+const _ = require('lodash')
+
+const platforms = [
+  'gmail',
+  'gmail-android',
+  'apple-mail',
+  'apple-ios',
+  'yahoo-mail',
+  'outlook',
+  'outlook-legacy',
+  'outlook-web',
+]
 
 module.exports = (context) => ({
   JSXOpeningElement: (node) => {
     const hasStyle = hasProp(node.attributes, 'style')
-    if (hasStyle) {
-      const element = elementType(node)
-      const prop = propName(getProp(node.attributes, 'style'))
-      const propValue = getPropValue(getProp(node.attributes, 'style'))
-      console.log(element, prop, propValue)
-      context.report({
-        node,
-        message: 'fuck'
-      })
-      // getProp(node.attributes, 'style')
+    if (!hasStyle) return
+
+    const componentName = elementType(node)
+    const styles = getPropValue(getProp(node.attributes, 'style'))
+
+    for (const style in styles) {
+      const unsupported = []
+      const warningMessages = []
+      const propName = _.kebabCase(style)
+      const supportInfo = supportMatrix[propName]
+
+      for (const platform of platforms) {
+        // if (typeof supportInfo[platform] === 'string') {
+        //   warningMessages.push(platform)
+        // }
+        if (!supportInfo[platform]) {
+          unsupported.push(platform)
+        }
+      }
+      // if (warningMessages.length > 0) {
+      //   context.report({
+      //     node,
+      //     message: `Warning: Style property \`${propName}\` supplied to \`${componentName}\`, in ${platforms.join(', ')}: ${msg.toLowerCase()}`
+      //   })
+      // }
+
+      if (unsupported.length > 0) {
+        context.report({
+          node,
+          message: `Style property \`${propName}\` supplied to \`${componentName}\` unsupported in: ${unsupported.join(', ')}.`
+        })
+      }
     }
   },
 })
--- src/rules/textAndFonts.spec.js
+++ src/rules/textAndFonts.spec.js
@@ -12,13 +12,17 @@ const ruleTester = new RuleTester({
 
 ruleTester.run('text-and-font', rule, {
   valid: [
-    { code: '<div style={{ direction: "ltr" }}>foo</div>' },
+    { code: '<div style={{ direction: "ltr", fontFamily: "Tahoma", fontSize: "14px" }}>foo</div>' },
   ],
 
   invalid: [
     {
       code: '<div style={{ textShadow: "1px black" }}>foo</div>',
-      errors: [ { message: 'Style property text-shadow supplied to div is unsupported in Gmail.' } ],
+      errors: [
+        { message: 'Style property `text-shadow` supplied to `div` unsupported in: gmail, outlook, outlook-legacy.' },
+        // { message: 'Style property `text-overflow` supplied to `div` unsupported in: outlook.' },
+
+      ],
     },
   ]
 })
