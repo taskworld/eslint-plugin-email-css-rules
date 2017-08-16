c598ba91d5e5e9903b20f0df8f9414a9f7febb83
Kawin Glomjai <kawin.g@taskworld.com>
2017-08-16 15:54:24 +0700

    test-case

--- src/rules/unsupported-css.spec.js
+++ src/rules/unsupported-css.spec.js
@@ -14,6 +14,16 @@ ruleTester.run('unsupported-css', rule, {
   valid: [
     { code: '<div style={{ direction: "ltr", fontFamily: "Tahoma", fontSize: "14px" }}>foo</div>' },
     { code: '<div style={{ textAlign: "center" }}>foo</div>' },
+    { code: `const MockModal = React.createClass({
+      render () {
+        return (<div style={{ textAlign: "center" }}>fooooo</div>)
+      }
+    })` },
+    { code: `class MockModal extends React.Component {
+      render () {
+        return (<div style={{ textAlign: "center" }}>fooooo</div>)
+      }
+    }` },
   ],
 
   invalid: [
@@ -33,6 +43,16 @@ ruleTester.run('unsupported-css', rule, {
       code: '<div style={{ backgroundSize: "black", borderTop: "1px" }}>foo</div>',
       errors: [
         { message: '`background-size` supplied to `div` is unsupported.' },
+      ],
+    },
+    {
+      code: `class MockModal extends React.Component {
+        render () {
+          return (<div style={{ textShadow: "center" }}>fooooo</div>)
+        }
+      }`,
+      errors: [
+        { message: '`text-shadow` supplied to `div` is unsupported.' },
       ],
     },
   ]
